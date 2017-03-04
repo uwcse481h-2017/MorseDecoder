29cf80a05fb7b330413a0d5b874f14db67deb976
TISHA TRONGTHAM <ttseattle@yahoo.com>
2017-03-03 22:40:55 -0800

    fixed bugs for menu, backspace, and progress bar

--- js/2Switch.js
+++ js/2Switch.js
@@ -35,7 +35,7 @@ var morseDictionary = {
 	"---..": "8",
 	"----.": "9",
 	"-----": "0",
-	"----" : toggleMenu
+	"----" : openMenu
 };
 
 // Switch constants 
@@ -53,7 +53,7 @@ var ESCS_DIVIDE;
 var CSWS_DIVIDE;
 
 // User's chosen speaking voice 
-var LANGUAGE;
+var LANGUAGE = "UK English Female";
 
 // Timing variables 
 var spaceTimer = null;
@@ -63,8 +63,8 @@ var timeouts = [];
 
 // Menu variables 
 var menuVisible = false;
-var menuCurrItem;
-var items;
+var menuCurrItem = -1;
+var items = $('.menuItem');
 
 // Keep track of word 
 var word = ""
@@ -87,75 +87,61 @@ $(document).ready(function() {
 	})
 	.then(function() {
 		$.get("/api/v1/getLanguage/" + userId, function(data) {
-			LANGUAGE = String(JSON.stringify(data.language));
-			LANGUAGE = LANGUAGE.slice(1, LANGUAGE.length-1)
-			console.log("language: " + LANGUAGE);
-
+			LANGUAGE = (String(JSON.stringify(data.language))).slice(1, -1);
 		});
 	})
 	.then(function() {
 		spaceTimer = new Stopwatch();
-	
-		menuCurrItem = 0;
-		items = $('.menuItem');
 
 		/* 
 		Listen for switch inputs 
 		*/
 		document.addEventListener("keydown", function(event) {
-			if (timerRunning && (event.which == DOT || event.which == DASH)) {
-				for (var i = 0; i < timeouts.length; i++) {
-			  		clearTimeout(timeouts[i]);
+			if (event.which == DOT || event.which == DASH) {
+				if (!menuVisible) {
+					resetProgressBar();
 				}
-
-				var spaceMs = spaceTimer.stop();
-				console.log(spaceMs.totalMs);
-				timerRunning = false;
-				
-				resetProgressBar();
+				if (timerRunning) {
+					for (var i = 0; i < timeouts.length; i++) {
+						clearTimeout(timeouts[i]);
+					}
+					var spaceMs = spaceTimer.stop();
+					timerRunning = false;
+				}
 			}
 		});
 
 		document.addEventListener("keyup", function(event) { 
 			if (event.which == DOT || event.which == DASH) {
-				$(".progress-bar").animate({
-				    width: "100%"
-				}, WORD_SPACE);
-
-				if (event.which == DOT) {
-					if(menuVisible) {
-						scroll();
-					} else {
-						word = append(word, ".");
-						breakStarted = false;
-						if(!menuOpen) {
-							resetTime();
-						}
-						
-					}
-				} else if (event.which == DASH) {
-					if(menuVisible) {
-						select();
-					} else {
-						word = append(word, "-");
-						breakStarted = false;
-						if(!menuOpen) {
-							resetTime();
-						}
-					}
+				if (!menuVisible) {
+					$(".progress-bar").animate({
+						width: "100%"
+					}, WORD_SPACE);
 				}
 			}
 
-			if (event.which == MENU) {
-				if(menuOpen) {
-					menuOpen = false;
+			if (event.which == DOT) {
+				if(menuVisible) {
+					scroll();
 				} else {
-					menuOpen = true;
+					word = append(word, ".");
+					breakStarted = false;
+					if(!menuOpen) {
+						resetTime();
+					}
 				}
-				toggleMenu();	
+			} else if (event.which == DASH) {
+				if(menuVisible) {
+					select();
+				} else {
+					word = append(word, "-");
+					breakStarted = false;
+					if(!menuOpen) {
+						resetTime();
+					}
+				}
 			}
 		});
-
 
 		/*
 		Add functions to buttons.
@@ -173,7 +159,7 @@ $(document).ready(function() {
 		});
 
 		$('#btn-close').click(function() {
-			toggleMenu();
+			closeMenu();
 		});
 
 		/*
@@ -187,15 +173,10 @@ $(document).ready(function() {
 
 // TRANSLATION ///////////////////////////////////////////////////////////
 
-function translate(needSpace) {
+function translate() {
 	if(word != MORSECODEMENU) {
 		$('#translation').append(morseDictionary[word]);
 	}
-	
-	if(needSpace) {
-		$('#translation').append(" ");
-	}
-	
 	getSuggestions();	
 	word = "";
 }
@@ -203,24 +184,13 @@ function translate(needSpace) {
 // PLAY //////////////////////////////////////////////////////////////////
 
 function play(sentence) {
-	if (LANGUAGE) {
-		responsiveVoice.speak(sentence, LANGUAGE);
-	} else { 
-		responsiveVoice.speak(sentence);
-	}
+	responsiveVoice.speak(sentence, LANGUAGE);
 }
 
 // BACKSPACE /////////////////////////////////////////////////////////////
 
 function backspace() {
-	console.log("backspace");
-	var str = $('#translation').text().trim();
-	console.log(str);
-
-	var sliced = str.slice(0,-1);
-	console.log(sliced);
-
-	$('#translation').text(sliced);
+	$('#translation').text($('#translation').text().slice(0, -1));
 }
 
 // TEXT SUGGESTIONS //////////////////////////////////////////////////////
@@ -273,33 +243,92 @@ function takeSuggestion() {
 	// else to go in the new sentence 
 	var allButLastWord = wordsWritten.slice(0, -1);
 	var newSentence = allButLastWord.join(" ");
-	if (allButLastWord.length > 1) {
+	if (allButLastWord.length > 0) {
 		newSentence += " ";
 	}
 
 	// get the suggested word and add it to the new sentence 
 	var suggestedWord = $('#suggestionBox').text().toUpperCase();
-	newSentence += suggestedWord + " "; 
+	newSentence += suggestedWord; 
 
 	// update the textbox with the new sentence
 	$('#translation').text(newSentence);
 }
 
+// PROGRESS BAR //////////////////////////////////////////////////////////
+
+function resetProgressBar() {
+	$(".progress-bar").stop(true, false);
+
+	$(".progress-bar").css({	  
+		'background-color': '#4CAF50'
+	});
+
+	$(".progress-bar").animate({
+	    width: "0%"
+	}, 0);
+	$(".progress-bar").stop(true, false);
+
+	$("#progressText").text("Element Space");
+}
+
+function stopProgressBar() {
+	$(".progress-bar").css({ width: "0%"});
+}
+
+function resetTime() {
+	spaceTimer.reset();
+	spaceTimer.start();
+	timerRunning = true; 
+
+	timeouts.push(setTimeout(function() { 
+		resetRealTimeText();
+		translate();
+		
+		$(".progress-bar").css({	 
+			'background-color': '#3F51B5'
+		});
+
+		$("#progressText").text("Character Space");
+	}, ESCS_DIVIDE));
+
+	timeouts.push(setTimeout(function() { 
+		translate(); 
+		addSpace();
+
+		$(".progress-bar").css({	  
+			'background-color': '#F44336'
+		});
+
+		$("#progressText").text("Word Space");
+	}, CSWS_DIVIDE));
+}
+
+function resetRealTimeText() {
+	$('#text').text('');
+	$('#correspondingWord').text('');
+	variableIndex = 0;
+}
+
 // MENU //////////////////////////////////////////////////////////////////
 
-/*
-Toggles the menu. 
-*/
-function toggleMenu() {
-    if(menuVisible) {
-    	menuVisible = false;
-		$('#menu-btn').removeClass('open');
-		$('.btn-option a').removeClass('active');
-		menuCurrItem = 0;
-    } else {
-    	menuVisible = true;		
-		$('#menu-btn').addClass('open');
-    } 
+function openMenu() {
+	menuVisible = true;		
+	$('#menu-btn').addClass('open');
+
+	resetProgressBar();
+}
+
+function closeMenu() {
+	menuVisible = false;
+	$('#menu-btn').removeClass('open');
+	$('.btn-option a').removeClass('active');
+	menuCurrItem = -1;
+
+	resetTime();
+	$(".progress-bar").animate({
+		width: "100%"
+	}, WORD_SPACE);
 }
 
 /**
@@ -312,16 +341,16 @@ function scroll() {
 	menuCurrItem++;
 
 	var mod = menuCurrItem % 4; 
-	if (mod == 1) {
+	if (mod == 0) {
 		$('.btn-option a').removeClass('active');
 		$('#btn-play a').addClass('active');
-	} else if (mod == 2) {
+	} else if (mod == 1) {
 		$('.btn-option a').removeClass('active');		
 		$('#btn-delete a').addClass('active');
-	} else if (mod == 3) {
+	} else if (mod == 2) {
 		$('.btn-option a').removeClass('active');		
 		$('#btn-close a').addClass('active');
-	}else if (mod == 0) {
+	}else if (mod == 3) {
 		$('.btn-option a').removeClass('active');		
 		$('#btn-suggest a').addClass('active');
 	} 
@@ -332,13 +361,13 @@ Selects underlined item in the menu.. must be scrolling
 */
 function select() {
 	var mod = menuCurrItem % 4; 
-	if (mod == 1) {
+	if (mod == 0) {
 		play($('#translation').text());
-	} else if (mod == 2) {
+	} else if (mod == 1) {
 		backspace();
+	} else if (mod == 2) {
+		closeMenu();
 	} else if (mod == 3) {
-		toggleMenu();
-	} else if (mod == 0) {
 		takeSuggestion();
 	}
 }
@@ -352,62 +381,16 @@ function append(morseCode, input) {
 
 	if(morseDictionary[morseCode]) {
 		if(morseCode == MORSECODEMENU) {
-			$('#correspondingWord').text("Menu Opened");
+			$('#correspondingWord').text("(MENU)");
 		} 
-			
 		$('#correspondingWord').text(morseDictionary[morseCode]);
 	}
 	
-	return morseCode ;
+	return morseCode;
 }
 
-function resetRealTimeText() {
-	$('#text').text('');
-	$('#correspondingWord').text('');
-	variableIndex = 0;
-}
-
-function resetTime() {
-	spaceTimer.reset();
-	spaceTimer.start();
-	timerRunning = true; 
-
-	timeouts.push(setTimeout(function() { 
-		resetRealTimeText();
-		translate(false); 
-		//$('#text').append("/"); 
-		//solidifyLetter(variableIndex);
-		
-		$(".progress-bar").css({	 
-			'background-color': '#3F51B5'
-		});
-
-		$("#progressText").text("Character Space");
-	}, ESCS_DIVIDE));
-
-	timeouts.push(setTimeout(function() { 
-		translate(true); 
-		//$('#text').append("_"); 
-
-		$(".progress-bar").css({	  
-			'background-color': '#F44336'
-		});
-
-		$("#progressText").text("Word Space");
-	}, CSWS_DIVIDE));
-}
-
-function resetProgressBar() {
-	//alert("stop");
-	$(".progress-bar").stop(true, false);
-	$(".progress-bar").css({	  
-		'background-color': '#4CAF50'
-	});
-
-	$(".progress-bar").animate({
-	    width: "0%"
-	}, 0);
-	$(".progress-bar").stop(true, false);
-
-	$("#progressText").text("Element Space");
+function addSpace() {
+	if ($('#translation').slice(-1) != " ") {
+		$('#translation').append(" ")
+	} 
 }
